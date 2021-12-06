49a99bcea51de547c2711201d25168f9275e2d2c
Yashwanth_Kumar_M_S <Yashwanth.KumarMS@analog.com>
2021-12-07 00:27:39 +0530

    The path to lua file should be absolute.

--- Code/Application/Main.c
+++ Code/Application/Main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
 
 int main()
 {
-    printf("Hello world");
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+    printf("hello from C");
+    luaL_dofile(L,"C:\\work\\Lua\\Code\\Application\\app.lua");
+    lua_close(L);
     return 0;
 }
